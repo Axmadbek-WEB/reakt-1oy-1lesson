9f3066c176cc8837f957f878f616fd013bb18fe0
Axmadbek-WEB <uzbytbs@gmail.com>
2025-02-04 14:53:07 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Reakt</h1>
+      <h2>Salomalekom bratva zelonaya</h2>
+      <ul>
+        <li>INdex</li>
+        <li>INdex</li>
+        <li>INdex</li>
+        <li>INdex</li>
+      </ul>
+      <h3>hayir hamaga</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At tempora, tempore beatae repudiandae vel voluptatem labore ipsa fugit dignissimos, repellendus, iure tenetur! Quia odio quis rem totam iure neque veniam blanditiis sed dolor, perferendis cum temporibus labore magnam voluptatibus omnis et dignissimos a eos consectetur tempora! Veritatis eligendi repudiandae atque dolores quis asperiores, reiciendis alias, officia fugiat, quo voluptate nisi molestias voluptatum saepe quibusdam iure mollitia porro voluptatem nobis? Iusto similique veniam velit nemo accusamus officiis fugit et, rerum, esse dignissimos cumque laboriosam hic, a inventore quam harum atque reprehenderit facere ut ullam laudantium eum? Nemo adipisci enim assumenda blanditiis aspernatur voluptas quia corrupti saepe, nihil ea illum repudiandae cupiditate magnam. Similique deleniti saepe accusamus ex quaerat dolor error. Dicta, reiciendis illum exercitationem obcaecati quia fugit ratione voluptates maiores ea, vitae fugiat soluta non similique. Obcaecati, consectetur! Quod autem sunt tenetur, minus possimus commodi sed, beatae illum temporibus harum hic eveniet omnis in provident quae natus. Amet unde dicta officiis, culpa voluptate iusto necessitatibus at enim pariatur ipsam rem repellat ea dolorum quasi soluta omnis, voluptatem magni cupiditate molestiae quod eligendi quibusdam! Vero temporibus laborum accusantium? Ut cum esse, animi inventore, a dolorem totam laudantium soluta quisquam ipsam maxime perspiciatis quae aliquid sit, nostrum labore expedita impedit officia explicabo? Nihil perspiciatis velit dolore, ipsa laudantium tempora. Laboriosam ducimus consequuntur voluptatibus vitae nobis iste</p>
+
+      <h3>hayir hamaga</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At tempora, tempore beatae repudiandae vel voluptatem labore ipsa fugit dignissimos, repellendus, iure tenetur! Quia odio quis rem totam iure neque veniam blanditiis sed dolor, perferendis cum temporibus labore magnam voluptatibus omnis et dignissimos a eos consectetur tempora! Veritatis eligendi repudiandae atque dolores quis asperiores, reiciendis alias, officia fugiat, quo voluptate nisi molestias voluptatum saepe quibusdam iure mollitia porro voluptatem nobis? Iusto similique veniam velit nemo accusamus officiis fugit et, rerum, esse dignissimos cumque laboriosam hic, a inventore quam harum atque reprehenderit facere ut ullam laudantium eum? Nemo adipisci enim assumenda blanditiis aspernatur voluptas quia corrupti saepe, nihil ea illum repudiandae cupiditate magnam. Similique deleniti saepe accusamus ex quaerat dolor error. Dicta, reiciendis illum exercitationem obcaecati quia fugit ratione voluptates maiores ea, vitae fugiat soluta non similique. Obcaecati, consectetur! Quod autem sunt tenetur, minus possimus commodi sed, beatae illum temporibus harum hic eveniet omnis in provident quae natus. Amet unde dicta officiis, culpa voluptate iusto necessitatibus at enim pariatur ipsam rem repellat ea dolorum quasi soluta omnis, voluptatem magni cupiditate molestiae quod eligendi quibusdam! Vero temporibus laborum accusantium? Ut cum esse, animi inventore, a dolorem totam laudantium soluta quisquam ipsam maxime perspiciatis quae aliquid sit, nostrum labore expedita impedit officia explicabo? Nihil perspiciatis velit dolore, ipsa laudantium tempora. Laboriosam ducimus consequuntur voluptatibus vitae nobis iste</p>
+    </>
+  )
+}
+
+export default App
